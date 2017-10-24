321c3714e0395e0873a32f3846669f44c254a541
Mikhalkov, Eugene <EMikhalkov@qulix.com>
2017-10-24 13:42:59 +0300

    height cell bug fixed

--- Gifer/GiferLayoutAttributes.swift
+++ Gifer/GiferLayoutAttributes.swift
@@ -1,14 +1,15 @@
-import UIKit
+//import UIKit
+//
+//class CustomLayoutAttributes: UICollectionViewLayoutAttributes {
+//    
+//    public var gifHeight: CGFloat = 0.0
+//    public var gifWidth: CGFloat = 0.0
+//    
+//    override func copy(with zone: NSZone?) -> Any {
+//        let copy = super.copy(with: zone) as! CustomLayoutAttributes
+//        copy.gifHeight = gifHeight
+//        copy.gifWidth = gifWidth
+//        return copy
+//    }
+//}
 
-class CustomLayoutAttributes: UICollectionViewLayoutAttributes {
-    
-    public var gifHeight: CGFloat = 0.0
-    public var gifWidth: CGFloat = 0.0
-    
-    override func copy(with zone: NSZone?) -> Any {
-        let copy = super.copy(with: zone) as! CustomLayoutAttributes
-        copy.gifHeight = gifHeight
-        copy.gifWidth = gifWidth
-        return copy
-    }
-}
